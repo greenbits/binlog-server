4312c883e1cc7ea83e9836aedbcb48f4d0036e93
bcurren <ben@green-bits.com>
2014-12-26 16:06:57 -0800

    added row event decorator

---
+++ src/main/java/com/greenbits/binlog/RowEventDecorator.java
@@ -0,0 +1,44 @@
+package com.greenbits.binlog;
+
+public class RowEventDecorator implements RowEventListener {
+    protected RowEventListener nextListener;
+    
+    public RowEventDecorator(RowEventListener nextListener) {
+        this.nextListener = nextListener;
+    }
+    
+    @Override
+    public void startup(ServerVersion version) {
+        nextListener.startup(version);
+    }
+
+    @Override
+    public void beginTransaction() {
+        nextListener.beginTransaction();
+    }
+
+    @Override
+    public void onUpdate(UpdateEvent updateEvent) {
+        nextListener.onUpdate(updateEvent);
+    }
+
+    @Override
+    public void onWrite(WriteEvent writeEvent) {
+        nextListener.onWrite(writeEvent);
+    }
+
+    @Override
+    public void onDelete(DeleteEvent deleteEvent) {
+        nextListener.onDelete(deleteEvent);
+    }
+    
+    @Override
+    public void onError(Throwable error) {
+        nextListener.onError(error);
+    }
+
+    @Override
+    public void commitTransaction() {
+        nextListener.commitTransaction();
+    }
+}
